c40122477a5ace679740a310b58f727a724c05e1
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-05-13 19:25:18 +0300

    added url parameters

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
@@ -10,21 +11,47 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
     private final RestTemplateBuilder restTemplateBuilder;
 
     private static final String GET_BEER_PATH = "/api/v1/beer";
+    private static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerID}";
 
     @Override
-    public Page<BeerDTO> listBeers(String beerName) {
+    public BeerDTO getBeerById(UUID beerId) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        return restTemplate.getForObject(GET_BEER_BY_ID_PATH, BeerDTO.class, beerId);
+    }
+
+    @Override
+    public Page<BeerDTO> listBeers() {
+        return this.listBeers(null, null, null, null, null);
+    }
+
+    @Override
+    public Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory,
+                                   Integer pageNumber, Integer pageSize) {
         RestTemplate restTemplate = restTemplateBuilder.build();
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEER_PATH);
 
-        if (beerName != null) {
+        if (beerName != null)
             uriComponentsBuilder.queryParam("beerName", beerName);
-        }
+
+        if (beerStyle != null)
+            uriComponentsBuilder.queryParam("beerStyle", beerStyle);
+
+        if (showInventory != null)
+            uriComponentsBuilder.queryParam("showInventory", showInventory);
+
+        if (pageNumber != null)
+            uriComponentsBuilder.queryParam("pageNumber", pageNumber);
+
+        if (pageSize != null)
+            uriComponentsBuilder.queryParam("pageSize", pageSize);
 
         ResponseEntity<BeerDTOPageImpl> response =
                 restTemplate.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,7 +14,21 @@ class BeerClientImplTest {
     BeerClientImpl beerClient;
 
     @Test
+    void getBeerById() {
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+        BeerDTO dto = beerDTOS.getContent().get(0);
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+
+        assertNotNull(byId);
+    }
+
+    @Test
+    void listBeersNoBeerName() {
+        beerClient.listBeers(null, null, null, null, null);
+    }
+
+    @Test
     void listBeers() {
-        beerClient.listBeers("ALE");
+        beerClient.listBeers("ALE", null, null, null, null);
     }
 }
